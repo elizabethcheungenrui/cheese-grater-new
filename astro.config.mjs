6e6b607c8754b90921efd515479302cf891248f2
Elizabeth Cheung <elizabethcheungenrui@gmail.com>
2026-01-14 22:35:02 +0000

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import react from '@astrojs/react';
 
 import mdx from '@astrojs/mdx';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()]
+  integrations: [react(), mdx()],
+  adapter: vercel()
 });
